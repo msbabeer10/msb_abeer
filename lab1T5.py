8d8a454112e828f705455cdae237af81a43b7332
msbabeer10 <massababeer07@gmail.com>
2026-01-29 20:45:10 +0500

    sb_abeer

---
+++ lab1T5.py
@@ -0,0 +1,23 @@
+list1 = [1, 4, 5, 6]
+list1.insert(4, 'ejaz')
+print(list1)
+print(list1[4][2])
+list1.append(2)
+list1.pop(2)
+list1.clear()
+print(list1)
+r = [1, 5, 4.5, "abeer"]
+print(r[-1:-4:-1])
+r.insert(4, "khan")
+
+print(r)
+r.remove(4.5)
+print(r)
+r.pop(3)
+print(r)
+r.clear()
+print(r)
+r = [4, 6, [9, 4]]
+print(r[2][1])
+r.append(3)
+print(r)
