65454f33d8491028686eacd977e3b020327c9d73
msbabeer10 <massababeer07@gmail.com>
2026-01-29 20:33:38 +0500

    sb_abeer

---
+++ lab1T2.py
@@ -0,0 +1,9 @@
+name = "   abeer khan   "
+print(name[0:11])
+firstname = "abeer"
+secondname = "khan"
+fullname = firstname + " " + secondname
+print(fullname)
+print(name.strip())
+print(fullname.upper())
+print(fullname.lower())
